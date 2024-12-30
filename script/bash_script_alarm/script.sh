fde66a31a8a7f7b92e464bf96b47fdff685086d7
davidseo98 <hyundukseo98@gmail.com>
2024-12-30 14:54:28 +0900

    [script] script alarm test code, test for multiple jobs

--- script/bash_script_alarm/script.sh
+++ script/bash_script_alarm/script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Microsoft Teams Webhook URL
 WEBHOOK_URL="Your webhook url, reference : https://learn.microsoft.com/ko-kr/azure/data-factory/how-to-send-notifications-to-teams?tabs=data-factory"
 
-# Function to send a message to Microsoft Teams
 send_teams_message() {
-    local MESSAGE=$1
-    curl -H "Content-Type: application/json" -X POST -d "{\"text\": \"$MESSAGE\"}" $WEBHOOK_URL
+    local EXIT_CODE=$1
+    local JOB_NAME=$2
+    local MESSAGE="$JOB_NAME has failed with exit code $EXIT_CODE"
+
+
+    if [ $EXIT_CODE -ne 0 ]; then
+        curl -H "Content-Type: application/json" -X POST -d "{\"text\": \"$MESSAGE\"}" $WEBHOOK_URL
+    fi
 }
 
-# Run the job
+# Execute job 1 and check exit code
 python job.py
-
-# Capture the exit code
 EXIT_CODE=$?
+send_teams_message $EXIT_CODE job1
 
-# Log the job status
-if [ $EXIT_CODE -eq 0 ]; then
-    send_teams_message "$(date) - job succeeded with exit code $EXIT_CODE"
-
-else
-    send_teams_message "job failed on $(hostname) with exit code $EXIT_CODE"
-fi
+# Execute job 2 and check exit code
+python job2.py
+EXIT_CODE=$?
+send_teams_message $EXIT_CODE job2
 
 exit $EXIT_CODE
